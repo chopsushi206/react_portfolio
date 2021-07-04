70c2ac4d81838bb9e1a760b8920e8ba7418c8c9c
LiHua Anderson <lihua.anderson@gmail.com>
2021-07-04 13:33:13 -0700

    added basic code to render components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
-  return <Router></Router>;
+  return (
+    <Router>
+      <Header />
+      <Nav />
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+  return (
+    <nav className="nav">
+      <span>Navigation</span>
+    </nav>
+  );
+}
+
+export default Nav;
